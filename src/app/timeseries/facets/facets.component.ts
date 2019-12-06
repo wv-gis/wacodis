83b6dc76a871b9e97d3ecf623c081c54a1bea4c6
Vkn <vkn@wupperverband.de>
2019-12-06 13:57:37 +0100

    added serviceUrl support and loading spinner

--- src/app/timeseries/facets/facets.component.ts
+++ src/app/timeseries/facets/facets.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ParameterFacetType, FacetSearchService } from '@helgoland/facet-search';
+import { InitializeService } from 'src/app/services/initialize.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'wv-facets',
   templateUrl: './facets.component.html',
   styleUrls: ['./facets.component.css']
 })
-export class FacetsComponent {
+export class FacetsComponent implements OnInit, OnDestroy{
+ 
 
   public categoryType: ParameterFacetType = ParameterFacetType.category;
   public featureType: ParameterFacetType = ParameterFacetType.feature;
@@ -16,9 +19,24 @@ export class FacetsComponent {
   public featureAutocomplete: string;
   public phenomenonAutocomplete: string;
 
+  public facetsActive: boolean;
+  public loading: boolean;
+
+  public subscriptions: Subscription[] = [];
+
   constructor(
-    public facetSearch: FacetSearchService
+    public facetSearch: FacetSearchService,
+    public initializeService: InitializeService
   ) { }
 
-
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
+  ngOnInit(): void {
+    // this.subscriptions.push(this.facetSearch.getResults().subscribe(res => this.facetsActive = this.facetSearch.areFacetsSelected()));
+    this.subscriptions.push(this.initializeService.getLoading().subscribe(loading => this.loading = loading));
+  }
+  public resetAll() {
+    // this.facetSearch.resetAllFacets();
+  }
 }
