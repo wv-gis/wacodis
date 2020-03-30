c3fa0f3c7eb8fd75661e8b190f898a9d94c6bffa
Vkn <vkn@wupperverband.de>
2020-03-30 16:25:25 +0200

    updated views to support leaflet

--- src/app/copernicus/products/vitality-view/vitality-view.component.ts
+++ src/app/copernicus/products/vitality-view/vitality-view.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { OlMapService } from '@helgoland/open-layers';
 import { RequestTokenService } from 'src/app/services/request-token.service';
 import BaseLayer from 'ol/layer/Base';
-import Layer from 'ol/layer/Layer';
+// import Layer from 'ol/layer/Layer';
 import { OSM, ImageArcGISRest, ImageWMS } from 'ol/source';
-import Map from 'ol/Map.js';
-import { Tile } from 'ol/layer';
+// import Map from 'ol/Map.js';
+// import { Tile } from 'ol/layer';
 import { ScaleLine } from 'ol/control';
 import ImageLayer from 'ol/layer/Image';
 import * as esri from 'esri-leaflet';
+import * as L from 'leaflet';
+import { MapCache, LayerOptions } from '@helgoland/map';
+import { ParameterFilter } from '@helgoland/core';
 
 const vitalityService = 'https://www.wms.nrw.de/umwelt/waldNRW';
 const WvG_URL = 'http://fluggs.wupperverband.de/secman_wss_v2/service/WMS_WV_Oberflaechengewaesser_EZG/guest?';
@@ -18,16 +21,16 @@ const WvG_URL = 'http://fluggs.wupperverband.de/secman_wss_v2/service/WMS_WV_Obe
   templateUrl: './vitality-view.component.html',
   styleUrls: ['./vitality-view.component.css']
 })
-export class VitalityViewComponent implements OnInit {
+export class VitalityViewComponent implements OnInit,AfterViewInit {
 
   public showZoomControl = true;
   public showAttributionControl = true;
-  public map: Map;
+  public map: L.Map;
 
-  public baselayers: BaseLayer[] = [];
-  public overviewMapLayers: Layer[] = [new Tile({
-    source: new OSM()
-  })];
+  public baselayers: L.Layer[] = [];
+  // public overviewMapLayers: Layer[] = [new Tile({
+  //   source: new OSM()
+  // })];
   public zoom = 14;
   public lat = 51.07;
   public lon = 7.21;
@@ -35,81 +38,145 @@ export class VitalityViewComponent implements OnInit {
   public token: string = '';
   public sentinelLayer: esri.ImageMapLayer;
   public mapId = 'vitality-map';
-
-
-  constructor(private mapService: OlMapService, private requestTokenSrvc: RequestTokenService) { }
+  public fitBounds: L.LatLngBoundsExpression = [[50.985, 6.924], [51.319, 7.607]];
+  public zoomControlOptions: L.Control.ZoomOptions = { position: 'topleft' };
+  public avoidZoomToSelection = false;
+  public baseMaps: Map<string, LayerOptions> = new Map<string, LayerOptions>();
+  public overlayMaps: Map<string, LayerOptions> = new Map<string, LayerOptions>();
+  public layerControlOptions: L.Control.LayersOptions = { position: 'bottomleft' };
+  public cluster = true;
+  public loadingStations: boolean;
+  public stationFilter: ParameterFilter = {
+    // phenomenon: '8'
+  };
+  public statusIntervals = false;
+  public mapOptions: L.MapOptions = { dragging: true, zoomControl: false };
+  public providerUrl: string = 'https://www.fluggs.de/sos2-intern-gis/api/v1/';
+  constructor(private mapService: OlMapService, private requestTokenSrvc: RequestTokenService, private mapCache: MapCache) { }
+  ngAfterViewInit(): void {
+    this.baselayers.forEach((lay,i,arr)=>{
+      this.mapCache.getMap(this.mapId).addLayer(lay);
+    });
+  }
 
   ngOnInit() {
-    this.mapService.getMap(this.mapId).subscribe((map) => {
-      map.getLayers().clear();
-      map.addControl(new ScaleLine({units: "metric"}));
-      map.addLayer(new Tile({
-        source: new OSM(),
-      }));
+  //   this.wmsLayer = L.tileLayer.wms('http://ows.terrestris.de/osm/service?',
+  //   {
+  //     layers: 'OSM-WMS', format: 'image/png', transparent: true, maxZoom: 16, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  //     className: 'OSM'
+  //   });
 
-      map.addLayer(new ImageLayer({
-        visible: true,
-        opacity: 0.8,
-        source: new ImageWMS({
-          url: WvG_URL,
-          params: {
-            'LAYERS': '0',
-          },
-        })
-      }));
+  // this.mainMap = L.map(this.mapId, this.mapOptions).setView([51.161, 7.482], 10);
 
-    });
-
-    this.baselayers.push(
-      new ImageLayer({
-        visible: false,
-        source: new ImageWMS({
-          attributions: "Datenlizenz Deutschland – Namensnennung – Version 2.0",
-          params: {
-            'LAYERS': 'nadelwald_2017_sep2108',
-          },
-          url: vitalityService
-        })
-      })
+  // this.mainMap.addLayer(this.wmsLayer);
+  // L.control.scale().addTo(this.mainMap);
+  // this.mapCache.setMap(this.mapId,this.mainMap);
+  this.baseMaps.set(this.mapId,
+    {
+        label: 'OSM-WMS', // will be shown in layer control
+        visible: true, // is layer by default visible
+        layer: L.tileLayer.wms(
+          'http://ows.terrestris.de/osm/service?',
+            {
+              layers: 'OSM-WMS', format: 'image/png', transparent: true, maxZoom: 16, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+              className: 'OSM'
+            }
+        )
+    }
+);
+    // var mousePosition = [];
+   this.baselayers.push( L.tileLayer.wms(vitalityService,
+    {
+      layers: 'nadelwald_06_207_09_2018', format: 'image/png', transparent: true, maxZoom: 16, attribution: 'Datenlizenz Deutschland – Namensnennung – Version 2.0',
+      className: 'nadel_2017_2018'
+    })
     );
 
-    this.baselayers.push(
-      new ImageLayer({
-        visible: false,
-        source: new ImageWMS({
-          attributions: "Datenlizenz Deutschland – Namensnennung – Version 2.0",
-          params: {
-            'LAYERS': 'nadelwald_2017_jun2019',
-          },
-          url: vitalityService
-        })
-      })
-    );
-    this.baselayers.push(
-      new ImageLayer({
-        visible: true,
-        source: new ImageWMS({
-          attributions: "Datenlizenz Deutschland – Namensnennung – Version 2.0",
-          params: {
-            'LAYERS': 'nadelwald_2017_aug2019',
-          },
-          url: vitalityService
-        })
-      })
-    );
+   
+   
+    // this.mapService.getMap(this.mapId).subscribe((map) => {
+    //   map.getLayers().clear();
+    //   map.addControl(new ScaleLine({units: "metric"}));
+    //   map.addLayer(new Tile({
+    //     source: new OSM(),
+    //   }));
 
-    this.baselayers.push(
-      new ImageLayer({
-        visible: false,
-        source: new ImageWMS({
-          attributions: "Datenlizenz Deutschland – Namensnennung – Version 2.0",
-          params: {
-            'LAYERS': 'windwurfschadflaechen_kyrill',
-          },
-          url: vitalityService
-        })
-      })
-    );
+    //   map.addLayer(new ImageLayer({
+    //     visible: true,
+    //     opacity: 0.8,
+    //     source: new ImageWMS({
+    //       url: WvG_URL,
+    //       params: {
+    //         'LAYERS': '0',
+    //       },
+    //     })
+    //   }));
+
+    // });
+
+    // this.baselayers.push(
+    //   new ImageLayer({
+    //     visible: false,
+    //     source: new ImageWMS({
+    //       attributions: "Datenlizenz Deutschland – Namensnennung – Version 2.0",
+    //       params: {
+    //         'LAYERS': 'nadelwald_06_207_09_2018',
+    //       },
+    //       url: vitalityService
+    //     })
+    //   })
+    // );
+
+    // this.baselayers.push(
+    //   new ImageLayer({
+    //     visible: false,
+    //     source: new ImageWMS({
+    //       attributions: "Datenlizenz Deutschland – Namensnennung – Version 2.0",
+    //       params: {
+    //         'LAYERS': 'nadelwald_06_2017_06_2019',
+    //       },
+    //       url: vitalityService
+    //     })
+    //   })
+    // );
+    // this.baselayers.push(
+    //   new ImageLayer({
+    //     visible: true,
+    //     source: new ImageWMS({
+    //       attributions: "Datenlizenz Deutschland – Namensnennung – Version 2.0",
+    //       params: {
+    //         'LAYERS': 'nadelwald_06_2017_08_2019',
+    //       },
+    //       url: vitalityService
+    //     })
+    //   })
+    // );
+
+    // this.baselayers.push(
+    //   new ImageLayer({
+    //     visible: false,
+    //     source: new ImageWMS({
+    //       attributions: "Datenlizenz Deutschland – Namensnennung – Version 2.0",
+    //       params: {
+    //         'LAYERS': 'windwurfschadflaechen_kyrill',
+    //       },
+    //       url: vitalityService
+    //     })
+    //   })
+    // );
+    
+    // this.baselayers.push(
+    //   new ImageLayer({
+    //     visible: false,
+    //     source: new ImageWMS({
+    //       attributions: "Datenlizenz Deutschland – Namensnennung – Version 2.0",
+    //       params: {
+    //         'LAYERS': 'waldtypen_real',
+    //       },
+    //       url: vitalityService
+    //     })
+    //   })
+    // );
 
    
   }
