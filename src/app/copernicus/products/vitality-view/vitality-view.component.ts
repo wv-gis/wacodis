ee00efaee965bdb9521bd7561ed0603b79036802
Vkn <vkn@wupperverband.de>
2020-03-31 15:54:43 +0200

    added extendedLeafletSupport for copernicusproducts

--- src/app/copernicus/products/vitality-view/vitality-view.component.ts
+++ src/app/copernicus/products/vitality-view/vitality-view.component.ts
@@ -11,11 +11,11 @@ import ImageLayer from 'ol/layer/Image';
 import * as esri from 'esri-leaflet';
 import * as L from 'leaflet';
 import { MapCache, LayerOptions } from '@helgoland/map';
-import { ParameterFilter } from '@helgoland/core';
+import { ParameterFilter, Station } from '@helgoland/core';
 
 const vitalityService = 'https://www.wms.nrw.de/umwelt/waldNRW';
 const WvG_URL = 'http://fluggs.wupperverband.de/secman_wss_v2/service/WMS_WV_Oberflaechengewaesser_EZG/guest?';
-
+const intraLandService = 'https://gis.wacodis.demo.52north.org:6443/arcgis/rest/services/WaCoDiS/EO_WACODIS_DAT_INTRA_LAND_COVER_CLASSIFICATION_Service/ImageServer';
 @Component({
   selector: 'wv-vitality-view',
   templateUrl: './vitality-view.component.html',
@@ -25,38 +25,28 @@ export class VitalityViewComponent implements OnInit,AfterViewInit {
 
   public showZoomControl = true;
   public showAttributionControl = true;
-  public map: L.Map;
 
   public baselayers: L.Layer[] = [];
   // public overviewMapLayers: Layer[] = [new Tile({
   //   source: new OSM()
   // })];
-  public zoom = 14;
   public lat = 51.07;
   public lon = 7.21;
+  public mainMap: L.Map;
 
-  public token: string = '';
-  public sentinelLayer: esri.ImageMapLayer;
   public mapId = 'vitality-map';
-  public fitBounds: L.LatLngBoundsExpression = [[50.985, 6.924], [51.319, 7.607]];
   public zoomControlOptions: L.Control.ZoomOptions = { position: 'topleft' };
   public avoidZoomToSelection = false;
-  public baseMaps: Map<string, LayerOptions> = new Map<string, LayerOptions>();
-  public overlayMaps: Map<string, LayerOptions> = new Map<string, LayerOptions>();
   public layerControlOptions: L.Control.LayersOptions = { position: 'bottomleft' };
-  public cluster = true;
-  public loadingStations: boolean;
-  public stationFilter: ParameterFilter = {
-    // phenomenon: '8'
-  };
-  public statusIntervals = false;
-  public mapOptions: L.MapOptions = { dragging: true, zoomControl: false };
+
+  public mapOptions: L.MapOptions = { dragging: true, zoomControl: true };
   public providerUrl: string = 'https://www.fluggs.de/sos2-intern-gis/api/v1/';
   constructor(private mapService: OlMapService, private requestTokenSrvc: RequestTokenService, private mapCache: MapCache) { }
   ngAfterViewInit(): void {
     this.baselayers.forEach((lay,i,arr)=>{
       this.mapCache.getMap(this.mapId).addLayer(lay);
     });
+   
   }
 
   ngOnInit() {
@@ -66,34 +56,47 @@ export class VitalityViewComponent implements OnInit,AfterViewInit {
   //     className: 'OSM'
   //   });
 
-  // this.mainMap = L.map(this.mapId, this.mapOptions).setView([51.161, 7.482], 10);
+  this.mainMap = L.map(this.mapId, this.mapOptions).setView([51.07, 7.21], 13);
 
-  // this.mainMap.addLayer(this.wmsLayer);
-  // L.control.scale().addTo(this.mainMap);
-  // this.mapCache.setMap(this.mapId,this.mainMap);
-  this.baseMaps.set(this.mapId,
-    {
-        label: 'OSM-WMS', // will be shown in layer control
-        visible: true, // is layer by default visible
-        layer: L.tileLayer.wms(
-          'http://ows.terrestris.de/osm/service?',
-            {
-              layers: 'OSM-WMS', format: 'image/png', transparent: true, maxZoom: 16, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-              className: 'OSM'
-            }
-        )
-    }
-);
-    // var mousePosition = [];
+  L.control.scale().addTo(this.mainMap);
+  this.mapCache.setMap(this.mapId,this.mainMap);
+
+  this.mainMap.addLayer(L.tileLayer.wms(
+    'http://ows.terrestris.de/osm/service?',
+      {
+        layers: 'OSM-WMS', format: 'image/png', transparent: true, maxZoom: 16, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        className: 'OSM'
+      }
+  ));
    this.baselayers.push( L.tileLayer.wms(vitalityService,
     {
-      layers: 'nadelwald_06_207_09_2018', format: 'image/png', transparent: true, maxZoom: 16, attribution: 'Datenlizenz Deutschland – Namensnennung – Version 2.0',
+      layers: 'nadelwald_06_207_09_2018', format: 'image/png', transparent: true, maxZoom: 16,minZoom:11, attribution: 'Datenlizenz Deutschland – Namensnennung – Version 2.0',
       className: 'nadel_2017_2018'
     })
     );
 
-   
-   
+
+    this.baselayers.push( L.tileLayer.wms(vitalityService,
+      {
+        layers: 'nadelwald_06_2017_06_2019', format: 'image/png', transparent: true, maxZoom: 16,minZoom:11, attribution: 'Datenlizenz Deutschland – Namensnennung – Version 2.0',
+        className: 'nadelwald_06_2017_06_2019', opacity: 0
+      })
+      );
+
+      this.baselayers.push( L.tileLayer.wms(vitalityService,
+        {
+          layers: 'nadelwald_06_2017_08_2019', format: 'image/png', transparent: true, maxZoom: 16,minZoom:11, attribution: 'Datenlizenz Deutschland – Namensnennung – Version 2.0',
+          className: 'nadelwald_06_2017_08_2019', opacity:0
+        })
+        );
+
+        this.baselayers.push( L.tileLayer.wms(vitalityService,
+          {
+            layers: 'waldtypen_real', format: 'image/png', transparent: true, maxZoom: 16,minZoom:11, attribution: 'Datenlizenz Deutschland – Namensnennung – Version 2.0',
+            className: 'waldtypen_real', opacity: 0
+          })
+          );
+
     // this.mapService.getMap(this.mapId).subscribe((map) => {
     //   map.getLayers().clear();
     //   map.addControl(new ScaleLine({units: "metric"}));
@@ -180,4 +183,5 @@ export class VitalityViewComponent implements OnInit,AfterViewInit {
 
    
   }
+ 
 }
