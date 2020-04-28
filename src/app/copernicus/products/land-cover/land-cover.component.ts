7224f9799b6a4d7646923747f74bf514cd2cec84
Vkn <vkn@wupperverband.de>
2020-04-28 16:46:06 +0200

    adjusted landcover layout to support responsiveness for bounds

--- src/app/copernicus/products/land-cover/land-cover.component.ts
+++ src/app/copernicus/products/land-cover/land-cover.component.ts
@@ -4,8 +4,7 @@ import * as esri from 'esri-leaflet';
 import { RequestTokenService } from 'src/app/services/request-token.service';
 import * as L from 'leaflet';
 import { MapCache } from '@helgoland/map';
-import Plotly from 'plotly.js-dist';
-import { locale } from 'src/environments/environment.prod';
+
 
 require('leaflet-timedimension');
 require('leaflet.sync');
@@ -46,155 +45,208 @@ export class LandCoverComponent implements OnInit, AfterViewInit {
   public selectedTime: number = 1;
   public selectedSyncTime: number = 1;
   public currentSelectedTimeL: Date = new Date();
-  public currentSelectedTimeR: Date= new Date();
+  public currentSelectedTimeR: Date = new Date();
 
   public syncmapOptions: L.MapOptions = { dragging: true, zoomControl: true, boxZoom: false };
-  public mapOptions: L.TimeDimensionMapOptions = { dragging: true, zoomControl: true, boxZoom: false , timeDimension: true, timeDimensionControl: false, 
-    timeDimensionControlOptions: {timeZones:['Local'],position: 'bottomleft'}};
+  public mapOptions: L.TimeDimensionMapOptions = {
+    dragging: true, zoomControl: true, boxZoom: false, timeDimension: true, timeDimensionControl: false,
+    timeDimensionControlOptions: { timeZones: ['Local'], position: 'bottomleft' }
+  };
   public wmsLayer: any;
   public mainMap: L.Map;
   public showDiagram: boolean = false;
-  public loading:boolean = false;
+  public loading: boolean = false;
   public defTimeL = 2;
   public defTimeR = 5;
+  public mapBounds: L.LatLngBounds;
 
-  constructor(private requestTokenSrvc: RequestTokenService, private mapCache: MapCache) {
+    constructor(private requestTokenSrvc: RequestTokenService, private mapCache: MapCache) {
 
-  }
+    }
+
   ngAfterViewInit(): void {
-    // this.mainMap.on('mouseup', this.identifyPixel, this);
-    // this.syncMap.on('mouseup', this.identifyPixel, this);
-  }
+      // this.mainMap.on('click', this.identifyPixel, this);
+      // this.syncMap.on('mouseup', this.identifyPixel, this);
+      this.mainMap.on('moveend',this.changeBounds,this);
+  
+    }
 
   ngOnInit() {
 
-    this.wmsLayer = L.tileLayer.wms('http://ows.terrestris.de/osm/service?',
-      {
-        layers: 'OSM-WMS', format: 'image/png', transparent: true, maxZoom: 16, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        className: 'OSM'
-      });
+      this.wmsLayer = L.tileLayer.wms('http://ows.terrestris.de/osm/service?',
+        {
+          layers: 'OSM-WMS', format: 'image/png', transparent: true, maxZoom: 16, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+          className: 'OSM'
+        });
 
-    this.mainMap = L.map(this.mapId, this.mapOptions).setView([51.180, 7.307], 13);
-    this.mainMap.timeDimension.setCurrentTime(new Date().getTime());
-    this.syncMap = L.map(this.syncMapId, this.syncmapOptions).setView([51.180, 7.307], 13);
+      this.mainMap = L.map(this.mapId, this.mapOptions).setView([51.180, 7.307], 13);
+      this.mapBounds= this.mainMap.getBounds();
+      this.mainMap.timeDimension.setCurrentTime(new Date().getTime());
+      this.syncMap = L.map(this.syncMapId, this.syncmapOptions).setView([51.180, 7.307], 13);
 
-    this.mainMap.addLayer(this.wmsLayer);
-    L.tileLayer.wms('http://ows.terrestris.de/osm/service?',
-      {
-        layers: 'OSM-WMS', format: 'image/png', transparent: true, maxZoom: 16, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        className: 'OSM'
-      }).addTo(this.syncMap);
-
-
-    L.control.scale().addTo(this.mainMap);
-    L.control.scale().addTo(this.syncMap);
-
-    this.mapCache.setMap(this.mapId, this.mainMap);
-    this.mapCache.setMap(this.syncMapId, this.syncMap);
-
-    this.sentinelLayer = esri.imageMapLayer({ url: intraLandService, opacity: 0.6, maxZoom: 16 });
-    // let testTimeLayer = new ExtendedOlLayerAnimateTimeComponent(esri.imageMapLayer(
-    // {
-    //   url: intraLandService, opacity: 0.8, maxZoom: 16
-    // }),{});
-    
-    this.syncSentinelLayer = esri.imageMapLayer({ url: intraLandService, opacity: 0.6, pane: 'imagePane' })
-    this.syncMap.createPane('imagePane');
-
-    this.mainMap.addLayer(this.sentinelLayer);
-    // this.mainMap.addLayer(testTimeLayer);
-    this.syncMap.addLayer(this.syncSentinelLayer);
-
-    this.mainMap.sync(this.syncMap);
-    this.syncMap.sync(this.mainMap);
-
-    this.baselayers.push(this.sentinelLayer);
-    this.syncBaselayers.push(this.syncSentinelLayer);
-
-    this.mainMap.invalidateSize();
-    this.syncMap.invalidateSize();
+      this.mainMap.addLayer(this.wmsLayer);
+      L.tileLayer.wms('http://ows.terrestris.de/osm/service?',
+        {
+          layers: 'OSM-WMS', format: 'image/png', transparent: true, maxZoom: 16, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+          className: 'OSM'
+        }).addTo(this.syncMap);
 
 
+      L.control.scale().addTo(this.mainMap);
+      L.control.scale().addTo(this.syncMap);
 
-  }
+      this.mapCache.setMap(this.mapId, this.mainMap);
+      this.mapCache.setMap(this.syncMapId, this.syncMap);
+
+      this.sentinelLayer = esri.imageMapLayer({ url: intraLandService, opacity: 0.6, maxZoom: 16 });
+      
+      // let testTimeLayer = new ExtendedOlLayerAnimateTimeComponent(esri.imageMapLayer(
+      // {
+      //   url: intraLandService, opacity: 0.8, maxZoom: 16
+      // }),{});
+
+      this.syncSentinelLayer = esri.imageMapLayer({ url: intraLandService, opacity: 0.6, pane: 'imagePane' })
+      this.syncMap.createPane('imagePane');
+
+      this.mainMap.addLayer(this.sentinelLayer);
+   
+      // this.mainMap.addLayer(testTimeLayer);
+      this.syncMap.addLayer(this.syncSentinelLayer);
+
+      this.mainMap.sync(this.syncMap);
+      this.syncMap.sync(this.mainMap);
+
+      this.baselayers.push(this.sentinelLayer);
+  
+      this.syncBaselayers.push(this.syncSentinelLayer);
+
+      this.mainMap.invalidateSize();
+      this.syncMap.invalidateSize();
+
+
+
+    }
   public setSelectedTime(num: number) {
-    this.selectedTime = num;
-  }
+      this.selectedTime = num;
+    }
   public setSelectedSyncTime(num: number) {
-    this.selectedSyncTime = num;
-  }
+      this.selectedSyncTime = num;
+    }
   public setSelectedCurrentTimeLeft(date: Date) {
-    this.currentSelectedTimeL = date;
-  }
+      this.currentSelectedTimeL = date;
+    }
   public setSelectedCurrentTimeRight(date: Date) {
-    this.currentSelectedTimeR = date;
+      this.currentSelectedTimeR = date;
 
-  }
+    }
+
+    public changeBounds(){
+
+      this.mapBounds= this.mainMap.getBounds();
+
+    }
 
   public identifyPixel(e) {
-    this.loading = true;
- 
+      // this.loading = true;
+
+      let identifiedPixel;
+      let dateVal: { date: Date, value: number }[] = [];
+      let dates: Date[] = [];
+      let values: number[] = [];
+      // let plotPane = this.plotlydiv.nativeElement;
+
+      if (this.sentinelLayer) {
+        // this.showDiagram = true;
+        // Plotly.register(locale);
+
+        this.sentinelLayer.metadata((err, metadata) => {
+          console.log(JSON.stringify(metadata));
+        })
+        this.sentinelLayer.bindPopup(function (error, identifyResults, response) {
+          if (error) {
+            console.log('Error: ' + error);
+            return;
+          } else {
+
+            // console.log('IdentifiedResults: '+ JSON.stringify(identifyResults));
+            // console.log('Response: ' + JSON.stringify(response));  
+
+            // this.loading = false;
+            // identifiedPixel = identifyResults.pixel.properties.values.reverse();
+            // identifyResults.catalogItems.features.forEach((f, i, arr) => {
+            //   dateVal.push({ date: new Date(f.properties.startTime), value: identifiedPixel[i] });
+            // });
+            // dateVal.sort(function (a, b) { return a.date.getTime() - b.date.getTime(); });
+            // dateVal.forEach((v, i, arr) => {
+            //   dates.push(v.date);
+            //   values.push(v.value);
+            // });
+
+            // let data = {
+            //   x: dates,
+            //   y: values,
+            //   mode: 'lines+markers',
+            //   type: 'scatter'
+            // };
+            // let layout = {
+            //   yaxis: {
+            //     title: {
+            //       text: 'Pixelwert',
+
+            //     },
+            //     showline: true,
+
+            //   },
+            //   xaxis: { showline: true },
+            //   height: 125,
+            //   // width: 800,
+            //   margin: { "t": 0, "b": 15, "l": 45, "r": 0 },
+            // };
+            // let config = {
+            //   toImageButtonOptions: {
+            //     format: 'png'
+            //   },
+            //   responsive: true,
+            //   locale: 'de',
+            //   displaylogo: false,
+            //   modeBarButtonsToRemove: ['select2d', 'lasso2d', 'hoverClosestCartesian',
+            //     'hoverCompareCartesian', 'toggleSpikelines', 'pan2d', 'zoomOut2d', 'zoomIn2d', 'autoScale2d', 'resetScale2d'],
+            // };
+
+            // Plotly.newPlot('pixelChart', [data], layout, config);
+          }
+        });
+        esri.imageService({url: intraLandService}).identify().at(e.latlng)
+        .between(this.currentSelectedTimeL,this.currentSelectedTimeR).returnCatalogItems(true).returnGeometry(true).run((er,featureCo,resp)=>{
+                console.log('FetaureCollectionIdentify: ' + JSON.stringify(featureCo));
+                console.log('FeatureCollectionIdentifyResp: '+ JSON.stringify(resp));
+              });
 
 
-    let identifiedPixel;
-    let dateVal: { date: Date, value: number }[] = [];
-    let dates: Date[] = [];
-    let values: number[] = [];
-    // let plotPane = this.plotlydiv.nativeElement;
-
-    if (this.sentinelLayer) {
-      this.showDiagram = true;
-      Plotly.register(locale);
-      this.sentinelLayer.bindPopup(function (error, identifyResults, response) {
-        if (error) {
-          console.log('Error: ' + error);
-          return;
-        } else {
-          this.loading = false;
-          identifiedPixel = identifyResults.pixel.properties.values.reverse();
-          identifyResults.catalogItems.features.forEach((f, i, arr) => {
-            dateVal.push({ date: new Date(f.properties.startTime), value: identifiedPixel[i] });
-          });
-          dateVal.sort(function (a, b) { return a.date.getTime() - b.date.getTime(); });
-          dateVal.forEach((v, i, arr) => {
-            dates.push(v.date);
-            values.push(v.value);
-          });
-
-          let data = {
-            x: dates,
-            y: values,
-            mode: 'lines+markers',
-            type: 'scatter'
-          };
-          let layout = {
-            yaxis: {
-              title: {
-                text: 'Pixelwert',
-
-              },
-              showline: true,
-
-            },
-            xaxis: { showline: true },
-            height: 125,
-            // width: 800,
-            margin: { "t": 0, "b": 15, "l": 45, "r": 0 },
-          };
-          let config = {
-            toImageButtonOptions: {
-              format: 'png'
-            },
-            responsive: true,
-            locale: 'de',
-            displaylogo: false,
-            modeBarButtonsToRemove: ['select2d', 'lasso2d', 'hoverClosestCartesian',
-              'hoverCompareCartesian', 'toggleSpikelines', 'pan2d', 'zoomOut2d', 'zoomIn2d', 'autoScale2d', 'resetScale2d'],
-          };
-
-          Plotly.newPlot('pixelChart', [data], layout, config);
-        }      
-      });
+      }
     }
+
+  private addMaskLayer(noDataMask: string[]){
+    // if(this.mapCache.getMap(this.mapId).hasLayer(esri.imageMapLayer(
+    //   {url: intraLandService,opacity: 0.4}
+    // ))){
+    //       this.mapCache.getMap(this.mapId).removeLayer(esri.imageMapLayer(
+    //         {url: intraLandService,opacity: 0.4}
+    //       ));
+    //     }
+
+    //   this.mapCache.getMap(this.mapId).addLayer(esri.imageMapLayer(
+    //     {url: intraLandService,opacity: 0.4,
+    //      renderingRule: {
+    //       "rasterFunction": "Mask",
+    //         "rasterFunctionArguments": {
+    //           "NoDataValues": ['1'],
+    //           "IncludedRanges": [0,24],
+    //           "NoDataInterpretation": 0
+    //         }, "variableName": "SelectedMask"
+    //       }}).bringToFront().setTimeRange(new Date((new Date(this.currentSelectedTimeL.getFullYear(),
+    //        this.currentSelectedTimeL.getMonth(), this.currentSelectedTimeL.getDate() + 2).getTime() - 2628000000))
+    //       , new Date(this.currentSelectedTimeL.getFullYear(), this.currentSelectedTimeL.getMonth(), this.currentSelectedTimeL.getDate() + 2)));
+    
+        }
   }
-}
