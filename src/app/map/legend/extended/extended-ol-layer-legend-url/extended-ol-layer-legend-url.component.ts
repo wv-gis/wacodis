1f19f985d3182a3b6d73943c8ccc618996a9aeee
Vkn <vkn@wupperverband.de>
2020-05-12 10:50:06 +0200

    update for legendUrl of different LayerTypes

--- src/app/map/legend/extended/extended-ol-layer-legend-url/extended-ol-layer-legend-url.component.ts
+++ src/app/map/legend/extended/extended-ol-layer-legend-url/extended-ol-layer-legend-url.component.ts
@@ -40,7 +40,7 @@ export class ExtendedOlLayerLegendUrlComponent {//extends OlLayerLegendUrlCompon
     //   const layerid = source.getParams()['layers'] || source.getParams()['LAYERS'];
     //   this.wmsCap.getLegendUrl(layerid, url).subscribe(res => this.legendUrl.emit(res));
     // }
-    
+ 
     if(this.layer.options.url){
       this.imageUrl = this.layer.options.url;
     
@@ -54,8 +54,8 @@ export class ExtendedOlLayerLegendUrlComponent {//extends OlLayerLegendUrlCompon
       esri.imageMapLayer({url: legendurl}).metadata((error,legendData)=>{
        legendData["layers"][0].legend.forEach((dat,i,arr)=>{
          if(arr[i].label.split('-').length > 1)
-          legendResp.push({url: "data:image/png;base64," + arr[i].imageData, label: arr[i].label.slice(3), layer:metadata["description"] }) ;
-      //  console.log(JSON.stringify(dat));
+          legendResp.push({url: "data:image/png;base64," + arr[i].imageData, label: arr[i].label, layer:metadata["description"] }) ;
+      
         });
         this.legendUrls.emit(legendResp);
         this.urls = legendResp;
@@ -65,16 +65,23 @@ export class ExtendedOlLayerLegendUrlComponent {//extends OlLayerLegendUrlCompon
   } else if(this.layer instanceof L.TimeDimension.Layer.WMS){
     this.url = this.layer.options.getCapabilitiesUrl;
     const layerid = this.layer.options.getCapabilitiesLayerName;
-      this.wmsCap.getLegendUrl(layerid, this.url).subscribe(res => {this.legendUrl.emit(res); this.legurl = res;});
+      this.wmsCap.getLegendUrl(layerid, this.url).subscribe(res => {this.legendUrl.emit(res); 
+        this.legurl = res;
+        this.urls =[{ url: res, label: "", layer: res.split('layer=')[1] }]
+      });
   }
     else{
       if(this.layer._url){
         this.url = this.layer._url;
+   
+        if(this.layer instanceof L.TileLayer.WMS){
+          const layerid = this.layer.wmsParams.layers;
+            this.wmsCap.getLegendUrl(layerid, this.url).subscribe(res => {this.legendUrl.emit(res); 
+              this.legurl = res;
+              this.urls =[{ url: res, label: "", layer: res.split('layer=')[1] }]});
+        }
       }
-      if(this.layer instanceof L.TileLayer.WMS){
-        const layerid = this.layer.wmsParams.layers;
-          this.wmsCap.getLegendUrl(layerid, this.url).subscribe(res => {this.legendUrl.emit(res); this.legurl = res;});
-      }
+     
     }
   }
 }
