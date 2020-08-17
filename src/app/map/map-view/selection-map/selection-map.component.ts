b4678e70f5c843512f51fc541d00debbdc960323
Vkn <vkn@wupperverband.de>
2020-08-17 14:34:21 +0200

    fix IE Error and legend depiction

--- src/app/map/map-view/selection-map/selection-map.component.ts
+++ src/app/map/map-view/selection-map/selection-map.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as esri from 'esri-leaflet';
 import { GeoSearchOptions, LayerOptions, MapCache } from '@helgoland/map';
-import {  SettingsService, Settings, HelgolandPlatform, HelgolandParameterFilter } from '@helgoland/core';
+import { SettingsService, Settings, HelgolandPlatform, HelgolandParameterFilter } from '@helgoland/core';
 import * as L from 'leaflet';
 import * as geojson from 'geojson';
 
@@ -70,9 +70,9 @@ export class SelectionMapComponent implements OnInit, AfterViewInit {
 
   }
 
-/**
- * set Basemap and its layers to show
- */
+  /**
+   * set Basemap and its layers to show
+   */
   ngOnInit() {
 
     this.baseMaps.set(this.mapId,
@@ -89,7 +89,7 @@ export class SelectionMapComponent implements OnInit, AfterViewInit {
       }
     );
 
-   
+
     esri.imageMapLayer({ url: wacodisUrl }).metadata((error, metadata) => {
       if (error) {
         console.log("Error with image service: " + error)
@@ -110,26 +110,60 @@ export class SelectionMapComponent implements OnInit, AfterViewInit {
         this.baselayers.forEach((blayer, i, arr) => {
           this.mapCache.getMap(this.mapId).addLayer(blayer);
           if (blayer instanceof esri.ImageMapLayer) {
-            if (blayer.options.alt == 'EO_WACODIS_DAT_VEGETATION_DENSITY_LAIService'){
-            
+            if (blayer.options.alt == 'EO_WACODIS_DAT_VEGETATION_DENSITY_LAIService') {
+
               blayer.setRenderingRule(
                 {
-                  "rasterFunction" : "Mask",
-                  "rasterFunctionArguments" : {
-                     "NoDataValues" : ["2.2250738585072014e-308"],
-                     "IncludedRanges" : [0,1],
-                     "NoDataInterpretation" : 0
-                     },    "variableName" : "Raster"
+                  "rasterFunction": "Mask",
+                  "rasterFunctionArguments": {
+                    "NoDataValues": ["2.2250738585072014e-308"],
+                    "IncludedRanges": [0, 1],
+                    "NoDataInterpretation": 0
+                  }, "variableName": "Raster"
                 }
               );
             }
-         
-      
+
+
           }
         });
         L.control.scale().addTo(this.mapCache.getMap(this.mapId));
       }
     });
+    let diffLayer = esri.imageMapLayer({
+      url: 'https://gis.wacodis.demo.52north.org:6443/arcgis/rest/services/WaCoDiS/EO_WACODIS_DAT_INTRA_LAND_COVER_CLASSIFICATIONService/ImageServer',
+      maxZoom: 16, opacity: 1, alt: 'DiffLandUse',bandIds: "1"
+    });
+    diffLayer.setRenderingRule(
+      {
+        "rasterFunction" : "Colormap",
+        "rasterFunctionArguments" : {
+          "Colormap" : [
+          [1, 255, 255, 0],
+          [2, 252, 200, 12],
+          [3, 50, 220, 255],
+          [4, 50, 220, 255],
+          [5, 153, 153, 153],
+          [6, 12, 252, 32],
+          [7, 12, 252, 32],
+          [8, 102, 63, 0],
+          [9, 252, 12, 12],
+          [10, 252, 12, 12],
+          [11, 229, 127, 255],
+          [12, 101, 178, 62],
+          [13, 62, 178, 72],
+          [14, 57, 127, 63],
+          [15, 178, 178, 62],
+          [16, 229, 160, 57],
+          [17, 255, 127, 208]              
+        ]
+      },
+      "variableName" : "Raster"
+      }
+    
+  );
+this.baselayers.push(diffLayer);
+// this.mapCache.getMap(this.mapId).addLayer(diffLayer);
   }
 
   /**
@@ -137,9 +171,9 @@ export class SelectionMapComponent implements OnInit, AfterViewInit {
    * @param station selected station
    */
   public onStationSelected(station: HelgolandPlatform) {
-    alert(station.label);
-    console.log(station);
+  alert(station.label);
+  console.log(station);
 
-  }
+}
 
 }
